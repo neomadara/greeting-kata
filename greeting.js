d8bffefd015c99a2273165b063001470287b6f61
Cristian Gutierrez <instanciageek@gmail.com>
2023-05-16 16:36:32 -0400

    sesion con gabriel

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
 function greet(name) {
-    if (name === null) return 'Hello, my friend.'
-    return 'Hello, ' + name + '.'
+    const greetText = name ?? 'my friend'
+    return  `Hello, ${greetText}.`
 }
 
 module.exports = greet;
--- greeting.test.js
+++ greeting.test.js
@@ -1,20 +1,22 @@
 const greet = require('./greeting')
 
-describe('Greeting Kata', () => {
-    test('Greet a single', () => {
-        const name = 'Cris'
-        const result = 'Hello, Cris.'
-
+describe('Greeting', () => {
+    test('Write a method greet(name) that interpolates name in a simple greeting', () => {
+        // arrange
+        const name = 'cris'
+        const result = 'Hello, cris.'
+        // act
         const greetResult = greet(name)
-
+        // assert
         expect(greetResult).toBe(result)
     })
-    test('Handle nulls', () => {
+    test('Handle nulls by introducing a stand-in', () => {
+        // arrange
         const name = null
         const result = 'Hello, my friend.'
-
+        // act
         const greetResult = greet(name)
-
+        // assert
         expect(greetResult).toBe(result)
     })
 })
